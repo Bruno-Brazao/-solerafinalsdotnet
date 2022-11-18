b2bafb08c1fde38be01f00272c7a20736d8df59f
José Azevedo <jose.azevedo@solera.com>
2022-11-18 11:29:07 +0100

    add Claim Controller

---
+++ hpels_mx/Controllers/ClaimsController.cs
@@ -0,0 +1,71 @@
+﻿using hpels_mx.Data;
+using hpels_mx.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace hpels_mx.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClaimsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ClaimsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Claims>> Get()
+        {
+            return await _context.Claims.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Object> Get(int id)
+        {
+            var owner = await _context.Claims.FirstOrDefaultAsync(m => m.Id == id);
+            if (owner == null)
+                return NotFound();
+            return Ok(owner);
+
+        }
+
+        [HttpPost]
+        public async Task<Object> Post(Claims claim)
+        {
+            _context.Add(claim);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<Object> Put(Claims claimData)
+        {
+            if (claimData == null)
+                return BadRequest();
+
+            var claim = await _context.Claims.FindAsync(claimData.Id);
+            if (claim == null)
+                return NotFound();
+            claim.Description = claimData.Description;
+            claim.Status = claimData.Status;
+            claim.Date = claimData.Date;
+            claim.Vehicle_id = claimData.Vehicle_id;
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<Object> Delete(int id)
+        {
+            var claim = await _context.Claims.FindAsync(id);
+            if (claim == null)
+                return NotFound();
+            _context.Claims.Remove(claim);
+            await _context.SaveChangesAsync();
+            return Ok();
+
+        }
+    }
+}
